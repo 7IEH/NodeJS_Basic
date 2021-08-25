bc7d436109c7b0f5af9d3b310513afe5fcdd9e96
7IEH <80614927+7IEH@users.noreply.github.com>
2021-08-25 22:21:57 +0900

    chapter 7

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const{ User }=require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+
+//application/json
+app.use(express.json());
+
 const mongoose=require('mongoose')
 mongoose.connect('mongodb+srv://7IEH:abcd1234@cluster0.fi9ky.mongodb.net/Cluster0?retryWrites=true&w=majority', {
   useNewUrlParser: true
@@ -9,9 +17,31 @@ mongoose.connect('mongodb+srv://7IEH:abcd1234@cluster0.fi9ky.mongodb.net/Cluster
   .catch(err=>console.err)
 
 
+
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요!')
 })
+
+
+app.post('/register',(req,res)=>{
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+ 
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+  
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
